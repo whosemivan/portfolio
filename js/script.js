dd55aa403d75944c1e535b2d06667a093fd55349
Ivan <vsem2004@gmail.com>
2021-11-27 16:58:43 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -5,11 +5,18 @@ const headerTitle = document.querySelector('.header__title');
 const mainLink = document.querySelector('.header__link');
 const header = document.querySelector('.header');
 
+const navImage = document.querySelector('.main-nav__image');
+const navList = document.querySelector('.main-nav__list');
+
 button.classList.remove('visually-hidden');
 nav.classList.add('visually-hidden');
+// navImage.classList.add('main-nav__image--hidden');
+// navList.classList.add('main-nav__list--hidden');
 
 button.addEventListener('click', function() {
   nav.classList.toggle('visually-hidden');
+  // navImage.classList.toggle('main-nav__image--hidden');
+  // navList.classList.toggle('main-nav__list--hidden');
   button.classList.toggle('header__btn--opened');
   header.classList.toggle('header--menu-open');
 });
@@ -19,6 +26,7 @@ mainLink.addEventListener('click', (evt) => {
   headerTitle.classList.add('header__title__wrapper--animation');
 
   setTimeout(() => {
+    headerTitle.classList.add('header__title--hidden-decorate-element');
     headerTitle.classList.add('header__title--decoration-element-animation');
   }, 2000)
 });
